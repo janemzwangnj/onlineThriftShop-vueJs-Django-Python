212cf86da16973283cafc3488f71293a1a5b1834
janemzwangnj <janemzwangnj@gmail.com>
2022-02-07 22:31:35 -0500

    created database thriftshop

---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE thriftshop;
+CREATE USER thriftshopuser WITH PASSWORD 'thriftshop';
+GRANT ALL PRIVILEGES ON DATABASE thriftshop TO thriftshopuser;
---
+++ thriftshop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ThriftshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'thriftshop'
